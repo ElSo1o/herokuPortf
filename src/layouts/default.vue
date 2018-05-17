d264840120d8cf34ffe5bcab620e00749859f757
ElSolo <ElSolo1991@mail.ru>
2018-05-17 22:08:01 +0300

    add new menu navigation

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,35 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <q-layout-drawer
+      side="right"
+      v-model="right"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-3' : null"
+    >
+        <q-list-header>Menu</q-list-header>
+      <q-item to="/home">
+        <q-item-side icon="account circle" />
+        <q-item-main label="Home" sublabel="About me" />
+      </q-item>
+        <q-item to="/skills">
+          <q-item-side icon="code" />
+          <q-item-main label="Skills" sublabel="What i use" />
+        </q-item>
+        <q-item to="/portfolio">
+          <q-item-side icon="view_array" />
+          <q-item-main label="Portfolio" sublabel="Examples of my work" />
+        </q-item>
+        <q-item to="/contacts">
+          <q-item-side icon="pin_drop" />
+          <q-item-main label="Contacts" sublabel="...on a Layout" />
+        </q-item>
+        <div v-if="scrolling" style="padding: 25px 16px 16px;">
+          <p class="caption" v-for="n in 50" :key="`left-${n}`">
+          </p>
+        </div>
+    </q-layout-drawer>
     <q-layout-header>
-      <quick-menu :menu-count=count @click="testFn" :background-color=backgroundColorMenu :color=color :icon-class=iconArr :menu-url-list=list :position=position :is-open-new-tab=false></quick-menu>
       <q-toolbar
+        side="right"
         color="grey-12"
         :glossy="false"
         :inverted="$q.theme === 'ios'"
@@ -12,6 +39,7 @@
           Elsolo's App
           <div slot="subtitle">Building for portfolio</div>
         </q-toolbar-title>
+        <q-btn flat round dense icon="menu" @click="right = !right" aria-label="Toggle menu on right side" />
       </q-toolbar>
     </q-layout-header>
 
@@ -23,19 +51,15 @@
 
 <script>
 import { openURL } from 'quasar'
-import quickMenu from 'vue-quick-menu'
 export default {
   name: 'LayoutDefault',
   components: {
-    quickMenu
   },
   data () {
     return {
+      scrolling: true,
+      right: false,
       leftDrawerOpen: this.$q.platform.is.desktop,
-      count: 4,
-      list: [{'isLink': true, url: '/doo'}, {'isLink': true, url: '/doo'}, {'isLink': true, url: '/doo'}, {'isLink': true, url: '/'}],
-      iconArr: ['fab fa-studiovinari', 'fab fa-connectdevelop', 'far fa-envelope-open', 'fas fa-terminal'],
-      position: 'top-right',
       isOpenNewTab: false,
       backgroundColorMenu: 'rgba(129, 191, 100, 0.9)',
       color: '#fff'
@@ -58,16 +82,4 @@ export default {
 </script>
 
 <style scoped>
-  .q-layout-header > .quick-menu{
-    z-index: 9;
-    top: 10px!important;
-  }
-  .q-toolbar{
-    z-index: 8;
-    padding: 4px 30px;
-    min-height: 80px;
-  }
-  .q-toolbar > .q-toolbar-title{
-    padding: 0;
-  }
 </style>
